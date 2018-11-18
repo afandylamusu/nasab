90dfb96e12e31fd665bb83e06152f309c3846e70
afandylamusu <afandy.lamusu@live.co.uk>
2018-11-18 12:43:27 +0700

    d3 js

--- src/Nasab.WebVue/ClientApp/router/routes.js
+++ src/Nasab.WebVue/ClientApp/router/routes.js
@@ -1,9 +1,11 @@
 import CounterExample from 'components/counter-example'
 import FetchData from 'components/fetch-data'
 import HomePage from 'components/home-page'
+import FamilyPage from 'components/family-page'
 
 export const routes = [
   { name: 'home', path: '/', component: HomePage, display: 'Home', icon: 'home' },
+  { name: 'family', path: '/family', component: FamilyPage, display: 'Family', icon: 'home' },
   { name: 'counter', path: '/counter', component: CounterExample, display: 'Counter', icon: 'graduation-cap' },
   { name: 'fetch-data', path: '/fetch-data', component: FetchData, display: 'Fetch data', icon: 'list' }
 ]
